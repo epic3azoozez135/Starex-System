98d4efc5ff1e4be794ae3f7dfca5ffce4a5f0622
Azoooooooooz <44898336+epic3azoozez135@users.noreply.github.com>
2018-12-18 23:37:07 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1088,7 +1088,7 @@ client.on("message", message => {
 
   client.on('message', message => {
               if(!message.channel.guild) return;
-    var prefix = "+";
+    var prefix = "#";
     if(message.content.startsWith(prefix + 'bc')) {
     if(!message.channel.guild) return message.channel.send('**هذا الأمر فقط للسيرفرات**').then(m => m.delete(5000));
   if(!message.member.hasPermission('ADMINISTRATOR')) return      message.channel.send('**للأسف لا تمتلك صلاحية** `ADMINISTRATOR`' );
